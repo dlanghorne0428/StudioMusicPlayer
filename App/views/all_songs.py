440ac457af4c8349693663c8cd139faa6bb0f29c
David Langhorne <david.b.langhorne@gmail.com>
2022-02-01 22:18:07 -0500

    comments and formatting

--- App/views/all_songs.py
+++ App/views/all_songs.py
@@ -8,10 +8,19 @@ from App.filters import SongFilter
 def all_songs(request):
     ''' shows all the Songs in the database. '''
     
+    # determine if the user is admin, pass that to template
     show_admin_buttons = request.user.is_superuser
 
+    # get the filtered list of Songs, ordered by title 
     songs = SongFilter(request.GET, queryset=Song.objects.all().order_by('title'))   
+    
+    # split the songs into pages and get the requested page
     paginator = Paginator(songs.qs, 16)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'all_songs.html', {'page_obj': page_obj, 'filter': songs, 'show_admin_buttons': show_admin_buttons })
+    
+    # render the template
+    return render(request, 'all_songs.html', 
+                  {'page_obj': page_obj, 
+                   'filter': songs, 
+                   'show_admin_buttons': show_admin_buttons })
