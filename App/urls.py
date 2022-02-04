bfb7bd56f01754541581d1189c8faf848e933b14
David Langhorne <david.b.langhorne@gmail.com>
2022-02-04 18:54:55 -0500

    Allow editing of playlists

--- App/urls.py
+++ App/urls.py
@@ -16,6 +16,10 @@ urlpatterns = [
     path('updateSong/<int:song_id>', views.update_song, name="update_song"),
     path('deleteSong/<int:song_id>', views.delete_song, name="delete_song"),
     path("playlists/", views.all_playlists, name="all_playlists"),
-    path("playlists/create/<int:sort_type>", views.create_playlist, name="create_playlist"),
+    path("playlists/create/<int:sort_type>", views.create_playlist, name="create_playlist"),    
+    path("playlists/edit/<int:playlist_id>", views.edit_playlist, name="edit_playlist"),
+    path("playlists/edit/<int:playlist_id>/up/<int:index>", views.edit_playlist_move_up, name="edit_playlist_move_up"),
+    path("playlists/edit/<int:playlist_id>/down/<int:index>", views.edit_playlist_move_down, name="edit_playlist_move_down"),    
+    path("playlists/edit/<int:playlist_id>/delete/<int:index>", views.edit_playlist_delsong, name="edit_playlist_delsong"),
     path('playSongList/<int:playlist_id>', views.play_song_list, name="play_song_list"),
 ]
--- App/views/playlist_crud.py
+++ App/views/playlist_crud.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
+from django.core.paginator import Paginator
 
 import os
 
@@ -41,3 +42,106 @@ def create_playlist(request, sort_type):
             index += 1
             
         return redirect('App:all_playlists')
+
+
+def edit_playlist(request, playlist_id):
+    ''' allows the superuser to edit an existing playlist. 
+       TODO: there should be a playlist owner, who can edit their own playlists.'''
+    
+    if not request.user.is_superuser:
+        return render(request, 'permission_denied.html')
+    else:
+        # get the specific playlist object from the database
+        playlist = get_object_or_404(Playlist, pk=playlist_id )
+        
+        # obtain list of songs in this playlist
+        song_list = playlist.songs.all().order_by('songinplaylist__order')
+        
+        # split the songs into pages and get the requested page
+        paginator = Paginator(song_list, 16)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)        
+        
+        return render(request, 'edit_playlist.html', {
+            'playlist': playlist,
+            'page_obj': page_obj
+        })  
+    
+def edit_playlist_move_up(request, playlist_id, index):
+    ''' allows the superuser to edit an existing playlist. 
+       The song at index is moved up one spot.
+       TODO: there should be a playlist owner, who can edit their own playlists.'''
+    
+    # get the specific playlist object from the database
+    playlist = get_object_or_404(Playlist, pk=playlist_id)
+    
+    # obtain list of songs in this playlist
+    song_list = playlist.songs.all
+    
+    print("index is: " + str(index))
+    
+    selected = SongInPlaylist.objects.get(playlist=playlist_id, order=index)
+    print(selected.song)
+    
+    previous = SongInPlaylist.objects.get(playlist=playlist_id, order=index - 1)
+    print(previous.song)
+    
+    selected.order = index - 1
+    selected.save()    
+    previous.order = index
+    previous.save()
+        
+    return redirect('App:edit_playlist', playlist_id)
+
+
+def edit_playlist_move_down(request, playlist_id, index):
+    ''' allows the superuser to edit an existing playlist.
+    The song at index is moved down one spot.
+       TODO: there should be a playlist owner, who can edit their own playlists.'''
+    
+    # get the specific playlist object from the database
+    playlist = get_object_or_404(Playlist, pk=playlist_id)
+    
+    # obtain list of songs in this playlist
+    song_list = playlist.songs.all
+    
+    print("index is: " + str(index))
+    
+    selected = SongInPlaylist.objects.get(playlist=playlist_id, order=index)
+    print(selected.song)
+    
+    next = SongInPlaylist.objects.get(playlist=playlist_id, order=index + 1)
+    print(next.song)
+    
+    selected.order = index + 1
+    selected.save()    
+    next.order = index
+    next.save()
+        
+    return redirect('App:edit_playlist', playlist_id)
+
+
+def edit_playlist_delsong(request, playlist_id, index):
+    ''' allows the superuser to edit an existing playlist. 
+    The song at index is deleted, all songs below move up one
+       TODO: there should be a playlist owner, who can edit their own playlists.'''
+    
+    # get the specific playlist object from the database
+    playlist = get_object_or_404(Playlist, pk=playlist_id)
+    
+    # obtain list of songs in this playlist
+    song_list = playlist.songs.all()
+    
+    print("index is: " + str(index))
+    
+    selected = SongInPlaylist.objects.get(playlist=playlist_id, order=index)
+    print("deleting: " + str(selected.song))
+    selected.delete()     
+    
+    for higher_index in range (index+1,len(song_list)):
+        next = SongInPlaylist.objects.get(playlist=playlist_id, order=higher_index)
+        next.order = higher_index - 1
+        print(next.song, next.order)
+        next.save()
+        
+    return redirect('App:edit_playlist', playlist_id)
