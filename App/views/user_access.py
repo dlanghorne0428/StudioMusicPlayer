32441b652b66d88840330d496be4ab862d872488
David Langhorne <david.b.langhorne@gmail.com>
2022-02-06 17:33:22 -0500

    Add views that control user signup

--- App/views/user_access.py
+++ App/views/user_access.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import CreateView, TemplateView
 
-###########################################
-# views for user login and logout
-###########################################
-def loginuser(request):
-    ''' allows an existing user to login.
-        currently new users must be added by the command line.'''
-    if request.method == "GET":
-        # display the built-in form to authenticate a user 
-        return render(request, 'login.html', {'form':AuthenticationForm()})
-    else:
-        # login an existing user, based on data obtained from the form
-        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
-        if user is None:
-            # authentication was unsuccessful, display error message on form
-            return render(request, 'login.html', {'form':AuthenticationForm(), 'error': "Invalid username or password"})
-        else:
-            # user found, log them in and return to the home page
-            login(request, user)
-            return redirect('App:home')
+from App.forms import TeacherSignUpForm
+from App.models import User
 
 
-def logoutuser(request):
-    ''' simply logout the current user and redirect to home page'''
-    logout(request)
-    return redirect('App:home') 
+# based on example from https://github.com/sibtc/django-multiple-user-types-example
+class SignUpView(TemplateView):
+    template_name = 'registration/signup.html'
+    
+
+class TeacherSignUpView(CreateView):
+    '''
+    This is a signup view for a teacher.
+    See https://simpleisbetterthancomplex.com/tutorial/2018/01/18/how-to-implement-multiple-user-types-with-django.html
+    '''
+    
+    # define the model, form, and template
+    model = User
+    form_class = TeacherSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        # add user_type = 'teacher' to the data being passed to the template
+        kwargs['user_type'] = 'teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        # save the user object returned by the form
+        user = form.save()
+        
+        # login the user that just signed up
+        login(self.request, user)
+        
+        # redirect based on settings - currently home page. TO_DO: show playlists owned by this user? 
+        return redirect(settings.LOGIN_REDIRECT_URL)
