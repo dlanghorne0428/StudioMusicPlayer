359c43878a6e1ebf4dc73f36d0efb789d0f7b77a
David Langhorne <david.b.langhorne@gmail.com>
2022-02-11 11:41:38 -0500

    Show playlists owned by user

--- App/urls.py
+++ App/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('updateSong/<int:song_id>', views.update_song, name="update_song"),
     path('deleteSong/<int:song_id>', views.delete_song, name="delete_song"),
     path("playlists/", views.all_playlists, name="all_playlists"),
+    path("playlists/<int:user_id>", views.all_playlists, name="all_playlists"),
     path("playlists/create/<int:sort_type>", views.create_playlist, name="create_playlist"),    
     path("playlists/edit/<int:playlist_id>", views.edit_playlist, name="edit_playlist"),
     path('playSongList/<int:playlist_id>', views.play_song_list, name="play_song_list"),
--- App/views/all_playlists.py
+++ App/views/all_playlists.py
@@ -1,25 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 
 # imported our models
 from App.models.playlist import Playlist
+from App.models.user import User
 
-def all_playlists(request):
+def all_playlists(request, user_id=None):
     ''' shows all the Playlists in the database. '''
+    
+    # assume there are no playlists
+    page_obj = None
     
     # if there are any playlists
     if Playlist.objects.count() > 0:
         
-        # get all the playlists, ordered by title
-        playlists = Playlist.objects.all().order_by('title')
+        if user_id is None:
+            # get all the playlists, ordered by title
+            playlists = Playlist.objects.all().order_by('title')
         
-        # split the playlists into pages and get the requested page
-        paginator = Paginator(playlists, 16)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
+        else:
+            # find user based on id
+            user = get_object_or_404(User, pk=user_id)
         
-    else:  # no playlists
-        page_obj = None
+            # get the playlists owned by that user
+            playlists = Playlist.objects.filter(owner=user).order_by('title')
+        
+        if len(playlists) > 0:
+            # split the playlists into pages and get the requested page
+            paginator = Paginator(playlists, 16)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
     
     # render the template
     return render(request, 'all_playlists.html', 
