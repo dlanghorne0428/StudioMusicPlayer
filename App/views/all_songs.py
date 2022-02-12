1db4a5e3bdef2dfe301c0944ea52af92e5f9c153
David Langhorne <david.b.langhorne@gmail.com>
2022-02-11 22:11:33 -0500

    Pass the user's playlists to tempate

--- App/views/all_songs.py
+++ App/views/all_songs.py
@@ -4,12 +4,16 @@ from django.core.paginator import Paginator
 # imported our models
 from App.models.song import Song
 from App.filters import SongFilter
+from App.models.playlist import Playlist
 
 def all_songs(request):
     ''' shows all the Songs in the database. '''
 
     # get the filtered list of Songs, ordered by title 
-    songs = SongFilter(request.GET, queryset=Song.objects.all().order_by('title'))   
+    songs = SongFilter(request.GET, queryset=Song.objects.all().order_by('title'))  
+    
+    # get all playlists owned by the current user
+    playlists = Playlist.objects.filter(owner=request.user).order_by('title')  
     
     # split the songs into pages and get the requested page
     paginator = Paginator(songs.qs, 16)
@@ -19,5 +23,6 @@ def all_songs(request):
     # render the template
     return render(request, 'all_songs.html', 
                   {'page_obj': page_obj, 
-                   'filter': songs}
-                  )
+                   'filter': songs,
+                   'playlists': playlists
+                  })
