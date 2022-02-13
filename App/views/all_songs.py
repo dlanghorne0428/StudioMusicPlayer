723b3400766ab28a6a20473e1ee644fc35599a6e
David Langhorne <david.b.langhorne@gmail.com>
2022-02-13 15:58:00 -0500

    bug fix
    only superusers and teachers have playlists

--- App/views/all_songs.py
+++ App/views/all_songs.py
@@ -12,8 +12,11 @@ def all_songs(request):
     # get the filtered list of Songs, ordered by title 
     songs = SongFilter(request.GET, queryset=Song.objects.all().order_by('title'))  
     
-    # get all playlists owned by the current user
-    playlists = Playlist.objects.filter(owner=request.user).order_by('title')  
+    playlists = None
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.is_teacher:
+            # get all playlists owned by the current user
+            playlists = Playlist.objects.filter(owner=request.user).order_by('title')  
     
     # split the songs into pages and get the requested page
     paginator = Paginator(songs.qs, 16)
