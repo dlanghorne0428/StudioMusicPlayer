4638d7791e9dea635bc9b9b3ae491d60b23cedf1
David Langhorne <david.b.langhorne@gmail.com>
2022-02-24 21:05:57 -0500

    Fix sorting

--- App/views/all_playlists.py
+++ App/views/all_playlists.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models.functions import Lower
 
 # imported our models
 from App.models.playlist import Playlist
@@ -15,15 +16,15 @@ def all_playlists(request, user_id=None):
     if Playlist.objects.count() > 0:
         
         if user_id is None:
-            # get all the playlists, ordered by title
-            playlists = Playlist.objects.all().order_by('owner', 'title')
+            # get all the playlists, ordered by owner's username then title
+            playlists = Playlist.objects.all().order_by(Lower('owner__username'), Lower('title'))
         
         else:
             # find user based on id
             user = get_object_or_404(User, pk=user_id)
         
             # get the playlists owned by that user
-            playlists = Playlist.objects.filter(owner=user).order_by('title')
+            playlists = Playlist.objects.filter(owner=user).order_by(Lower('title'))
         
         if len(playlists) > 0:
             # split the playlists into pages and get the requested page
