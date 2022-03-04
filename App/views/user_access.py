363239c9003176cf6fa1c81123309563290e5005
David Langhorne <david.b.langhorne@gmail.com>
2022-03-03 21:48:04 -0500

    support holiday themed playlists

--- App/views/user_access.py
+++ App/views/user_access.py
@@ -6,7 +6,7 @@ from django.views.generic import CreateView, TemplateView
 
 from App.forms import RandomPlaylistForm, TeacherSignUpForm
 from App.models import User
-from App.models.song import DANCE_TYPE_DEFAULT_PERCENTAGES
+from App.models.song import DANCE_TYPE_DEFAULT_PERCENTAGES, HOLIDAY_DEFAULT_USAGE
 
 
 def user_profile(request):
@@ -20,51 +20,72 @@ def user_preferences(request):
     user = request.user
     playlist = None
     
-    # if user has percentage preferences, use those, otherwise use defaults
-    if user.percentage_preferences is None:
-        percentage_preferences = DANCE_TYPE_DEFAULT_PERCENTAGES
+    # if user has preferences, use those, otherwise use defaults
+    if user.preferences is None:
+        preferences = {
+            'playlist_length'            : 10,
+            'prevent_back_to_back_styles': True,
+            'prevent_back_to_back_tempos': True,
+            'percentages'                : DANCE_TYPE_DEFAULT_PERCENTAGES,
+            'holiday_usage'              : HOLIDAY_DEFAULT_USAGE,
+        }
     else:
-        percentage_preferences = user.percentage_preferences
+        preferences = user.preferences
+        
     if request.method == "GET":
         # build and render the random playlist form
-        form = RandomPlaylistForm(prefs= percentage_preferences)
+        form = RandomPlaylistForm(prefs=preferences)
         return render(request, 'build_random_playlist.html', {
             'form': form, 
             'playlist': playlist,
         })
     else:  # POST
-        submit = request.POST.get("submit")
-        # if user hit cancel button during build random, delete playlist that was in process of being created. 
-        if submit == "Cancel": 
-            return redirect('App:all_playlists', user.id) 
+        cancel = request.POST.get("cancel")
+        if cancel == "Cancel": 
+            return redirect('App:user_preferences') 
         
         # obtain data from form and make sure it is valid.
-        form = RandomPlaylistForm(request.POST,prefs=percentage_preferences)
+        form = RandomPlaylistForm(request.POST,prefs=preferences)
         if form.is_valid():
             form_data = form.cleaned_data
         else:
             # TODO: handle this error 
             print('invalid');
         
-        # set variables based on form data    
-        #playlist_length = form_data['number_of_songs']
-        #prevent_back_to_back_styles = form_data['prevent_back_to_back_styles']
-        #prevent_back_to_back_tempos = form_data['prevent_back_to_back_tempos']
-
-        # get percentages entered by the user from the form
-        starting_percentages = dict()
-        for key in DANCE_TYPE_DEFAULT_PERCENTAGES:
-            form_field = '%s_pct' % (key, )
-            starting_percentages[key] = form_data[form_field]
+        # determine if the inputs should be saved as user's new default preferences
+        if form_data['save_preferences']:        
+        
+            # set variables based on form data    
+            playlist_length = form_data['number_of_songs']
+            prevent_back_to_back_styles = form_data['prevent_back_to_back_styles']
+            prevent_back_to_back_tempos = form_data['prevent_back_to_back_tempos']
+    
+            # get percentages entered by the user from the form
+            starting_percentages = dict()
+            for key in DANCE_TYPE_DEFAULT_PERCENTAGES:
+                form_field = '%s_pct' % (key, )
+                starting_percentages[key] = form_data[form_field]
             
-        # determine if the inputs should be saved as user's new default percentages
-        if form_data['save_preferences']:
-            user.percentage_preferences = starting_percentages
+            # get holiday usage data from the form    
+            holiday_usage = dict()
+            for key in HOLIDAY_DEFAULT_USAGE:
+                form_field = "%s_use" % (key, )
+                holiday_usage[key] = form_data[form_field]
+            
+            # update the preferences dictionary
+            preferences['playlist_length'] = playlist_length
+            preferences['prevent_back_to_back_styles'] = prevent_back_to_back_styles
+            preferences['prevent_back_to_back_tempos'] = prevent_back_to_back_tempos
+            preferences['percentages'] = starting_percentages
+            preferences['holiday_usage'] = holiday_usage
+            user.preferences = preferences
             user.save()
             
-        # TODO: need to indicate data was saved somehow     
-        return redirect('App:all_playlists', user.id) 
-
+            # TODO: need to indicate data was saved somehow     
+            return redirect('App:all_playlists', user.id) 
+        
+        else:  # save box was not checked
+            return redirect('App:user_preferences') 
 
 # based on example from https://github.com/sibtc/django-multiple-user-types-example
 class SignUpView(TemplateView):
