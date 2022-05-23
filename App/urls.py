301c70bb071a6ec8a3ff54649a72e63d2fb902ab
David Langhorne <david.b.langhorne@gmail.com>
2022-05-22 20:16:15 -0400

    Add more spotify URLs

--- App/urls.py
+++ App/urls.py
@@ -32,7 +32,11 @@ urlpatterns = [
     path('spotify/followed-artists', views.spotify_followed_artists, name="spotify_followed_artists"),
     path('spotify/recently-played', views.spotify_recently_played, name="spotify_recently_played"),
     path('spotify/saved-albums', views.spotify_saved_albums, name="spotify_saved_albums"),
+    path('spotify/saved-playlists', views.spotify_saved_playlists, name="spotify_saved_playlists"),
+    path('spotify/search', views.spotify_search, name="spotify_search"),
     path("spotify/artist-albums/<str:artist_id>", views.spotify_artist_albums, name="spotify_artist_albums"),
+    path("spotify/artist-tracks/<str:artist_id>", views.spotify_artist_tracks, name="spotify_artist_tracks"),
     path("spotify/album-tracks/<str:album_id>", views.spotify_album_tracks, name="spotify_album_tracks"),
+    path("spotify/playlist-tracks/<str:playlist_id>", views.spotify_playlist_tracks, name="spotify_playlist_tracks"),
     path("spotify/import-track/<str:track_id>", views.add_spotify_track, name="add_spotify_track"),
 ]
