49996e6de31acbf874f142fcc4e8b9685168a92b
David Langhorne <david.b.langhorne@gmail.com>
2022-05-27 23:03:06 -0400

    Move user options from navbar to home page
    Button links and text status headings more user-friendly than dropdown menu

--- App/views/home.py
+++ App/views/home.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     ''' The current home page for this app.'''
-    return render(request,"home.html")
+    return render(request,"home.html", {'user': request.user})
