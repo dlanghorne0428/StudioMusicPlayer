272f117368ab3845ea2dd5af6714b78d336ac914
David Langhorne <david.b.langhorne@gmail.com>
2022-06-04 19:12:15 -0400

    Use spotify web player to stream single song

--- App/views/play_song.py
+++ App/views/play_song.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 # imported our models
 from App.models.song import Song
+from App.views.spotify_views import spotify_token 
 
 # Create your views here
 
@@ -20,8 +21,17 @@ def play_song(request, song_id):
     default_url = settings.STATIC_URL + "img/default.png"
     
     if song.spotify_track_id is not None:
+        
+        token_dict = spotify_token(request.user)
+        if token_dict is None:
+            return render(request, 'not_signed_in_spotify.html')
+        
+        spotify_uris = list()
+        spotify_uris.append(song.spotify_uri())
+                
         return render(request, "play_spotify_song.html", 
-                  {'track_id': song.spotify_track_id,
+                  {'spotify_uris': spotify_uris,
+                   'user_token': token_dict['access_token'],
                    'title': song.title,
                    'artist': song.artist,
                    'cover_art': song.image_link,
