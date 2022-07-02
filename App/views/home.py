7fabc10ea4ca89ddd9ae1733d20f9511ce39b623
David Langhorne <david.b.langhorne@gmail.com>
2022-07-02 17:03:48 -0400

    Allow graceful exit
    would not be used for web-hosted version

--- App/views/home.py
+++ App/views/home.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+import sys
 
 # Create your views here.
 def home(request):
     ''' The current home page for this app.'''
     return render(request,"home.html", {'user': request.user})
+
+def exit(request):
+    sys.exit("Operator terminated program")
+    
