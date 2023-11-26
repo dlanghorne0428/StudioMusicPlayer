79adede1964e74c2b82d404a876dee7a749d426b
David Langhorne <david.b.langhorne@gmail.com>
2023-11-26 18:47:07 -0500

    Iss 227 play spotify song (#228)
    * Allow General songs to be displayed
    
    enables user to assign a dance type after Spotify load
    
    * Play Spotify track by ID
    
    Use our single-song player
    
    * Use single-song player instead of spotify preview

--- App/filters.py
+++ App/filters.py
@@ -6,7 +6,7 @@ class SongFilter(django_filters.FilterSet):
 
     artist = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':20}))
     title = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':20}))
-    dance_type = django_filters.ChoiceFilter(choices=DANCE_TYPE_CHOICES[:-1], empty_label="Any") 
+    dance_type = django_filters.ChoiceFilter(choices=DANCE_TYPE_CHOICES, empty_label="Any") 
     
     class Meta:
         model = Song
