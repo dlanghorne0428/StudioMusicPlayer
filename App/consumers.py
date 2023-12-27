dd30ad00f4bee00e117d00aa67e0a0e1cc84e11a
David Langhorne <david.b.langhorne@gmail.com>
2023-12-26 19:49:39 -0500

    only send now_playing message to group

--- App/consumers.py
+++ App/consumers.py
@@ -32,13 +32,28 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        logger.debug('message received from websocket')
+        logger.debug(text_data_json)
         
-        # Send message to group
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name, {"type": "now_playing.message", "message": message}
-        )
+        # if this is a "now_playing message
+        if text_data_json["type"] == 'now_playing':
+            message = text_data_json["message"]
+            logger.info('message received from websocket')
+        
+            # Send message to group
+            async_to_sync(self.channel_layer.group_send)(
+                self.group_name, {"type": "now_playing.message", "message": message}
+            )
+        # if this is a like message
+        elif text_data_json["type"] == 'like':
+            song_id = text_data_json["song_id"]
+            # TODO: need to update statistics for this song
+            logger.info("User liked song " + song_id)
+        # if this is a dislike message
+        elif text_data_json["type"] == 'hate':
+            song_id = text_data_json["song_id"]
+            logger.info("User hated song " + song_id)
+        else:
+            logger.warning("Other message type received")
 
     # Receive message from group
     def now_playing_message(self, event):
