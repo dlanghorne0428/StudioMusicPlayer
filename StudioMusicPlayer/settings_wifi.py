9ac8ae1997f8afd2678951014f2b69d6c401ea44
David Langhorne <david.b.langhorne@gmail.com>
2023-12-31 18:20:52 -0500

    add fxc-mini-pc as host
    and any ip addresses on local subnet

--- StudioMusicPlayer/settings_wifi.py
+++ StudioMusicPlayer/settings_wifi.py
@@ -1,7 +1,8 @@
 
 from StudioMusicPlayer.settings import *
 
-ALLOWED_HOSTS = ['davids-imac.local', '192.168.1.247', 'tara-alienware.local', '192.168.1.171', 'localhost']
+ALLOWED_HOSTS = ['davids-imac.local', 'FXC-Mini-PC.local', 'Studio-Laptop.local', 'tara-alienware.local', 'localhost']
+ALLOWED_HOSTS += ['192.168.1.{}'.format(i) for i in range(256)]
 
 INSTALLED_APPS.insert(0, 'daphne')
 INSTALLED_APPS.append('channels')
