0f37e7f2ed783f461ac62f612f92325e6d7ab909
David Langhorne <david.b.langhorne@gmail.com>
2023-12-31 18:21:28 -0500

    Use docker start command
    comment out run command - only needed once

--- StudioMusicPlayer_wifi.bat
+++ StudioMusicPlayer_wifi.bat
@@ -1,4 +1,5 @@
-docker run --name=my_redis -p 6379:6379 -d redis
+REM docker run --name=my_redis -p 6379:6379 -d redis
+docker start my_redis
 start firefox.exe http://localhost:8000
 py manage.py runserver --noreload --settings=StudioMusicPlayer.settings_wifi 0.0.0.0:8000
 taskkill /im firefox.exe
