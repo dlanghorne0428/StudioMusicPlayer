6a4b471720cbc4c971bdf72403104eab593a5dfb
David Langhorne <david.b.langhorne@gmail.com>
2023-12-31 12:07:19 -0500

    separate batch files for wifi

---
+++ StudioMusicPlayer_wifi.bat
@@ -0,0 +1,4 @@
+docker run --name=my_redis -p 6379:6379 -d redis
+start firefox.exe http://localhost:8000
+py manage.py runserver --noreload --settings=StudioMusicPlayer.settings_wifi 0.0.0.0:8000
+taskkill /im firefox.exe
