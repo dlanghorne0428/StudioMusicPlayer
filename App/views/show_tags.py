e11b7b39966268856ee5135f4db5a746697e0202
David Langhorne <david.b.langhorne@gmail.com>
2024-03-24 20:38:26 -0400

    add, remove, and show tags for song

--- App/views/show_tags.py
+++ App/views/show_tags.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.conf import settings
+from django.db.models.functions import Lower
 
 # imported our models
+from App.models.song import Song, Tagged_Song
 from App.models.tag import Tag
 from App.forms import TagEntryForm
 
@@ -96,3 +98,84 @@ def edit_tag(request, tag_id):
         'button_title': button_title,
         })       
 
+
+def show_tags_for_song(request, song_id):
+    ''' shows the current tags applied to a song and all other available tags'''    
+    # must be a valid user to view tags
+    if not request.user.is_authenticated:
+        logger.warning(request.user.username + " not authorized to view song tags")
+        return render(request, 'permission_denied.html')   
+    
+    # find the specific song object
+    song = get_object_or_404(Song, pk=song_id) 
+    logger.info("Showing tags for " +  str(song))  
+    
+    if song.image_link is None:
+        print('Song image: ' + str(song.image))
+        if song.image:
+            cover_art = song.image.url
+        else:
+            cover_art = settings.STATIC_URL + 'img/default.png'
+    else:
+        cover_art = song.image_link 
+        
+    current_tags = Tagged_Song.objects.filter(song=song)
+    print(current_tags)
+    
+    all_tags = Tag.objects.all().order_by(Lower('title'))
+    avail_tags = list()
+    for t in all_tags:
+        found = False
+        for ct in current_tags:
+            if ct.tag == t:
+                found = True
+                break;
+        if not found:
+            avail_tags.append(t)
+    
+    for t in avail_tags:
+        print(t.title)
+    
+    # render the template
+    return render(request, 'update_song_tags.html', {
+        'song': song, 
+        'cover_art': cover_art,
+        'available_tags': avail_tags,
+        'current_tags': current_tags,
+        })          
+
+
+def add_tag_to_song(request, tag_id, song_id):
+    ''' adds a tag to a song ''' 
+    if not request.user.is_authenticated:
+        logger.warning(request.user.username + " not authorized to add song tags")
+        return render(request, 'permission_denied.html')   
+    
+    # find the specific song object and tag object
+    song = get_object_or_404(Song, pk=song_id) 
+    tag = get_object_or_404(Tag, pk=tag_id) 
+    logger.info("Adding tag " + str(tag) + " to " +  str(song)) 
+    
+    ts = Tagged_Song()
+    ts.song = song
+    ts.tag = tag
+    ts.save()
+    
+    return redirect('App:show_tags_for_song', song_id)
+
+def remove_tag_from_song(request, tagged_song_id):
+    ''' adds a tag to a song ''' 
+    if not request.user.is_authenticated:
+        logger.warning(request.user.username + " not authorized to add song tags")
+        return render(request, 'permission_denied.html')   
+    
+    # find the specific song object and tag object
+    tagged_song = get_object_or_404(Tagged_Song, pk=tagged_song_id) 
+    
+    # save song id for redirect
+    song_id = tagged_song.song.id
+    
+    logger.info("Removing tag " + str(tagged_song.tag.title) + " from " +  str(tagged_song.song.title)) 
+    tagged_song.delete()
+    
+    return redirect('App:show_tags_for_song', song_id)
