7ae572c876ceded883c0ce2e3bd3b0aba99caedb
David Langhorne <david.b.langhorne@gmail.com>
2025-11-29 12:24:21 -0500

    custom filter both dance type fields
    match either dance_type or alt_dance_type

--- App/filters.py
+++ App/filters.py
@@ -1,14 +1,23 @@
 import django_filters
 from django import forms
+from django.db.models import Q
 from .models.song import Song, DANCE_TYPE_CHOICES
 
 class SongFilter(django_filters.FilterSet):
 
     artist = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':20}))
     title = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'size':20}))
-    dance_type = django_filters.ChoiceFilter(choices=DANCE_TYPE_CHOICES, empty_label="Any") 
+
+    # define custom filter to cover both dance type and alternate dance type
+    q = django_filters.ChoiceFilter(method='custom_dance_type_filter', choices=DANCE_TYPE_CHOICES, empty_label="Any") 
     
     class Meta:
         model = Song
-        fields = ['artist', 'title', 'dance_type']
+        fields = ['artist', 'title', 'q']
+        
+    # custom filter allows either dance type field to match
+    def custom_dance_type_filter(self, queryset, name, value):
+        return queryset.filter (
+            Q(dance_type=value) | Q (alt_dance_type=value)
+        )
                   
